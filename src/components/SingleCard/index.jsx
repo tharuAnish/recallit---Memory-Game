8a7f19efa315468d580617e3a3141f8bfbd7eae6
tharuAnish <namoanishtharu@gmail.com>
2022-12-19 19:56:21 +0530

    matching and flipping the cards

--- src/components/SingleCard/index.jsx
+++ src/components/SingleCard/index.jsx
@@ -1,19 +1,21 @@
 import "./singlecard.style.css"
 
-export default function SingleCard({ card, handleChoice }) {
+export default function SingleCard({ card, handleChoice, flipped }) {
   const handleClick = () => {
-    handleChoice(card) // for knowing which card is clicked
+    handleChoice(card)
   }
 
   return (
     <div className="card">
-      <img src={card.src} className="cardFront" alt="image" />
-      <img
-        src="./img/cover.webp"
-        onClick={handleClick}
-        className="cardBack"
-        alt="cover image"
-      />
+      <div className={flipped ? "flipped" : " "}>
+        <img src={card.src} className="cardFront" alt="image" />
+        <img
+          src="./img/cover.webp"
+          onClick={handleClick}
+          className="cardBack"
+          alt="cover image"
+        />
+      </div>
     </div>
   )
 }
